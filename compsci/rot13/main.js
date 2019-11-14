51f43f86e79f32473c9eef21bb51ca2823113943
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-11-14 13:42:00 -0500

    rot13

---
+++ compsci/rot13/main.js
@@ -0,0 +1,39 @@
+import readline from "readline";
+
+function main() {
+  readline.createInterface({
+    input: process.stdin
+  }).on("line", s => {
+    console.log(rot13(s));
+  });
+}
+
+export function rot13(s) {
+  return Array.from(s).map(c => rotateChar(c, 13)).join("");
+}
+
+export function rotateChar(c, n) {
+  let base;
+  if (c >= "A" && c <= "Z") {
+    base = ord("A");
+  } else if (c >= "a" && c <= "z") {
+    base = ord("a");
+  } else {
+    return c;
+  }
+  const i = ord(c) - base;
+
+  return String.fromCharCode(base + rotateIndex(i, n));
+}
+
+export function rotateIndex(i, n) {
+  return (i + n) % 26;
+}
+
+function ord(c) {
+  return c.charCodeAt(0);
+}
+
+if (process.env.NODE_ENV !== "test") {
+  main();
+}
---
+++ compsci/rot13/main.test.js
@@ -0,0 +1,40 @@
+import {
+  rot13,
+  rotateChar,
+  rotateIndex
+} from "./main.js";
+
+test.each([
+  ["SERR PBQR PNZC", "FREE CODE CAMP"],
+  ["SERR CVMMN!", "FREE PIZZA!"],
+  ["SERR YBIR?", "FREE LOVE?"],
+  ["GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.",
+   "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."]
+])("rot13(\"%s\")", (s, expected) => {
+  expect(rot13(s)).toBe(expected);
+});
+
+test.each([
+  ["A", 1, "B"],
+  ["c", 3, "f"],
+  ["Z", 1, "A"],
+  ["b", 26, "b"],
+  ["b", 53, "c"],
+  ["M", 13, "Z"],
+  ["Z", 13, "M"],
+  ["?", 10, "?"]
+])("rotateChar(\"%s\", %d)", (c, n, expected) => {
+  expect(rotateChar(c, n)).toBe(expected);
+});
+
+test.each([
+  [0, 1, 1],
+  [2, 3, 5],
+  [25, 1, 0],
+  [1, 26, 1],
+  [1, 53, 2],
+  [12, 13, 25],
+  [25, 13, 12]
+])("rotateIndex(%d, %d)", (i, n, expected) => {
+  expect(rotateIndex(i, n)).toBe(expected);
+});
