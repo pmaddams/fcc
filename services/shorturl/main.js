f0b5903f242f11fbe595acceaa9831b07a1ba00e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-11-29 19:24:17 -0500

    shorturl

--- services/shorturl/main.js
+++ services/shorturl/main.js
@@ -1,3 +1,5 @@
+import url from "url";
+
 import compression from "compression";
 import express from "express";
 import googleapis from "googleapis";
@@ -5,37 +7,37 @@ import helmet from "helmet";
 import mongodb from "mongodb";
 import fetch from "node-fetch";
 
+const cache = namespace(() => new Map(), "users", "urls", "ids");
+
+const errors = namespace(name => Symbol(name), "limit", "valid", "malware");
+
 function main() {
   const app = createServer();
 
-  app.get("/api/shorturl/new/:url", async (req, res) => {
+  app.post("/api/shorturl/new/:url", async (req, res) => {
     try {
-      await checkURL(req.params.url, req.ip);
+      const id = await checkURL(req.params.url, req.ip);
+      return res.json({
+        original_url: req.params.url,
+        short_url: encode(id || (await setURL(req.params.url)))
+      });
     } catch (err) {
-      switch (err.constructor) {
-        case RateLimitError:
-          break;
-        case ParseError:
-          break;
-        case DuplicateError:
-          break;
-        case ReachableError:
-          break;
-        case MalwareError:
-          break;
+      switch (err.type) {
+        case errors.limit:
+        case errors.valid:
+        case errors.malware:
+          return res.json({ error: err.message });
         default:
           throw err;
       }
     }
-    return res.json({
-      original_url: url,
-      short_url: encode(await writeURL(req.params.url))
-    });
   });
 
-  app.get("/api/shorturl/:id", async (req, res) =>
-    res.redirect(301, await readURL(decode(req.params.id)))
-  );
+  app.get("/api/shorturl/:id", async (req, res) => {
+    const n = decode(req.params.id);
+    const s = cache.ids.get(n) || (await getURL(n));
+    return s ? res.redirect(301, s) : res.sendStatus(404);
+  });
 
   app.listen(process.env.PORT || 3000);
 }
@@ -68,38 +70,40 @@ export function createServer() {
   });
 }
 
-function checkURL(url, ip) {}
-
-class CustomError extends Error {
-  constructor(...args) {
+class URLError extends Error {
+  constructor(type, ...args) {
     super(...args);
-    this.name = this.constructor.name;
+    this.type = type;
   }
 }
 
-class RateLimitError extends CustomError {}
+async function checkURL(s, ip) {
+  // ...
+  cache.users.set(ip, checkLimit(cache.users.get(ip)));
+  await checkValid(s);
+  await checkMalware;
+}
 
-function checkRateLimit() {}
+export function checkLimit(
+  history,
+  now = Date.now(),
+  count = 3,
+  interval = 60000
+) {
+  if (history.len >= count - 1 && now - history[0] < interval) {
+    throw new URLError(errors.limit, "limit exceeded");
+  }
+  history.push(n);
+  return history.slice(1, count);
+}
 
-class ParseError extends CustomError {}
-
-function checkParse() {}
-
-class DuplicateError extends CustomError {}
-
-function checkDuplicate() {}
-
-class ReachableError extends CustomError {}
-
-function checkReachable() {}
-
-class MalwareError extends CustomError {}
+function checkValid() {}
 
 function checkMalware() {}
 
-function writeURL(url) {}
+function setURL(s) {}
 
-function readURL(id) {}
+function getURL(n) {}
 
 export function encode(n) {
   return n.toString(36);
@@ -107,6 +111,10 @@ export function encode(n) {
 
 export function decode(s) {
   return parseInt(s, 36);
+}
+
+export function namespace(f, ...args) {
+  return Object.freeze(Object.fromEntries(args.map(x => [x, f(x)])));
 }
 
 if (process.env.NODE_ENV !== "test") {
