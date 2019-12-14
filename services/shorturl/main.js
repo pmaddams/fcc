f56ce24b87f2ada86e96f25588817042c18de497
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-12-13 22:55:40 -0500

    shorturl

--- services/shorturl/main.js
+++ services/shorturl/main.js
@@ -3,23 +3,28 @@ import { URL } from "url";
 import compression from "compression";
 import express from "express";
 import helmet from "helmet";
+import sqlite3 from "sqlite3";
 
 function main() {
   const app = createServer();
+  const db = openDatabase(process.env.DB);
 
   app.post("/api/shorturl/new", async (req, res) => {
     try {
       return res.json({
         original_url: req.body.url,
-        short_url: encode(await setURL(req.body.url))
+        short_url: encode(await setURL(db, req.body.url))
       });
     } catch (err) {
+      if (err instanceof URLError) {
         return res.json({ error: err.message });
+      }
+      throw err;
     }
   });
 
   app.get("/api/shorturl/:id", async (req, res) => {
-    const url = await getURL(decode(req.params.id));
+    const url = await getURL(db, decode(req.params.id));
 
     return url ? res.redirect(301, url) : res.sendStatus(404);
   });
@@ -55,11 +60,30 @@ export function createServer() {
   });
 }
 
-async function setURL(url) {
+export function openDatabase(file = ":memory:") {
+  return new sqlite3.Database(file, err => {
+    if (err) {
+      throw err;
+    }
+  });
 }
 
-async function getURL(id) {
+class URLError extends Error {
+  constructor(...args) {
+    super(...args);
+    this.name = this.constructor.name;
+  }
 }
+
+async function setURL(db, url) {
+  try {
+    new URL(url);
+  } catch (err) {
+    throw new URLError("invalid URL");
+  }
+}
+
+async function getURL(db, id) {}
 
 export function encode(n) {
   return n.toString(36);
