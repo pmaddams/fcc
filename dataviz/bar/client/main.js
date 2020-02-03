22b108a479dc63220022552bc3b818ee7a993134
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-02-03 16:25:03 -0500

    bar

--- dataviz/bar/client/main.js
+++ dataviz/bar/client/main.js
@@ -1,6 +1,18 @@
 import * as d3 from "d3";
 
-function main() {}
+const url =
+  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+function main() {
+  getData(url).then();
+}
+
+async function getData(url) {
+  return (await d3.json(url)).data.map(p => ({
+    date: new Date(p[0]),
+    gdp: p[1]
+  }));
+}
 
 if (process.env.NODE_ENV !== "test") {
   main();
