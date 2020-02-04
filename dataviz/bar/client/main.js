56f4f5ba7cce1af980c757fc0e8d32ccd0bd90bb
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-02-03 19:02:52 -0500

    bar

--- dataviz/bar/client/main.js
+++ dataviz/bar/client/main.js
@@ -2,9 +2,40 @@ import * as d3 from "d3";
 
 const url =
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+const viewBoxWidth = 300;
+const viewBoxHeight = 150;
 
 function main() {
-  getData(url).then();
+  getData(url).then(data => {
+    d3.select("body")
+      .append("h1")
+      .attr("id", "title")
+      .text("U.S. Gross Domestic Product");
+
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("viewBox", `0 0 ${viewBoxWidth} ${viewBoxHeight}`);
+
+    const scaleX = d3
+      .scaleLinear()
+      .domain([0, data.length])
+      .range([0, viewBoxWidth]);
+
+    const scaleY = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.gdp)])
+      .range([viewBoxHeight, 0]);
+
+    svg
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d, i) => scaleX(i))
+      .attr("y", d => scaleY(d.gdp))
+      .attr("width", viewBoxWidth / data.length)
+      .attr("height", viewBoxHeight);
+  });
 }
 
 async function getData(url) {
