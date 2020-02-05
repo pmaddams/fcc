970c79e6ecbd9be5459c21c40217ca0b18892c00
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-02-04 22:06:36 -0500

    bar

--- dataviz/bar/client/main.js
+++ dataviz/bar/client/main.js
@@ -2,8 +2,10 @@ import * as d3 from "d3";
 
 const url =
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-const viewBoxWidth = 300;
-const viewBoxHeight = 150;
+const viewBoxWidth = 1200;
+const paddingWidth = 0.05 * viewBoxWidth;
+const viewBoxHeight = 600;
+const paddingHeight = 0.05 * viewBoxHeight;
 
 function main() {
   getData(url).then(data => {
@@ -21,12 +23,29 @@ function main() {
     const scaleX = d3
       .scaleLinear()
       .domain([0, data.length])
-      .range([0, viewBoxWidth]);
+      .range([paddingWidth, viewBoxWidth - paddingWidth]);
 
     const scaleY = d3
       .scaleLinear()
       .domain([0, d3.max(data, d => d.gdp)])
-      .range([viewBoxHeight, 0]);
+      .range([viewBoxHeight - paddingHeight, paddingHeight]);
+
+    const scaleHeight = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.gdp)])
+      .range([0, viewBoxHeight - 2 * paddingHeight]);
+
+    const scaleYears = d3
+      .scaleLinear()
+      .domain([
+        data[0].date.getUTCFullYear(),
+        data[data.length - 1].date.getUTCFullYear()
+      ])
+      .range([paddingWidth, viewBoxWidth - paddingWidth]);
+
+    const dataWidth = (viewBoxWidth - 2 * paddingWidth) / data.length;
+    const dataBarWidth = 0.75 * dataWidth;
+    const dataPaddingWidth = 0.25 * dataWidth;
 
     svg
       .selectAll("rect")
@@ -35,11 +54,23 @@ function main() {
       .attr("class", "bar")
       .attr("data-date", d => d.date.toUTCString())
       .attr("data-gdp", d => d.gdp)
-      .attr("x", (d, i) => scaleX(i))
+      .attr("x", (d, i) => scaleX(i) + dataPaddingWidth / 2)
       .attr("y", d => scaleY(d.gdp))
-      .attr("width", (0.75 * viewBoxWidth) / data.length)
-      .attr("height", viewBoxHeight)
+      .attr("width", dataBarWidth)
+      .attr("height", d => scaleHeight(d.gdp))
       .on("mouseover", d => d3.select("#tooltip").text(tooltip(d)));
+
+    svg
+      .append("g")
+      .attr("id", "x-axis")
+      .attr("transform", `translate(0, ${viewBoxHeight - paddingHeight})`)
+      .call(d3.axisBottom(scaleYears).tickFormat(d3.format("d")));
+
+    svg
+      .append("g")
+      .attr("id", "y-axis")
+      .attr("transform", `translate(${paddingWidth}, 0)`)
+      .call(d3.axisLeft(scaleY));
 
     figure.append("figcaption").attr("id", "tooltip");
   });
