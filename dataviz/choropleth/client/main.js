a63e2a08c002d26ea8a5f781455fa10ebfe07d76
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-02-14 22:12:39 -0500

    choropleth

--- dataviz/choropleth/client/main.js
+++ dataviz/choropleth/client/main.js
@@ -1,16 +1,17 @@
 import * as d3 from "d3";
+import * as topojson from "topojson-client";
 
 const topologyURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 const metadataURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-const viewBoxWidth = 720;
+const viewBoxWidth = 975;
 const paddingWidth = 0.05 * viewBoxWidth;
-const viewBoxHeight = 360;
+const viewBoxHeight = 610;
 const paddingHeight = 0.05 * viewBoxHeight;
 
 function main() {
-  getData(topologyURL, metadataURL).then(data => {
+  getData(topologyURL, metadataURL).then(({ topology, metadata }) => {
     d3.select("body")
       .append("h1")
       .attr("id", "title")
@@ -26,6 +27,37 @@ function main() {
     const svg = figure
       .append("svg")
       .attr("viewBox", `0 0 ${viewBoxWidth} ${viewBoxHeight}`);
+
+    const scale = d3
+      .scaleQuantile()
+      .domain(
+        Array.from(metadata.values())
+          .map(v => v.graduated)
+          .sort()
+      )
+      .range([
+        ...Array(5).fill("highly-ignorant"),
+        ...Array(2).fill("ignorant"),
+        ...Array(2).fill("educated"),
+        "highly-educated"
+      ]);
+
+    svg
+      .append("g")
+      .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(topology, topology.objects.counties).features)
+      .join("path")
+      .attr("class", d => `county ${scale(metadata.get(d.id).graduated)}`)
+      .attr("data-fips", d => d.id)
+      .attr("data-education", d => metadata.get(d.id).graduated)
+      .attr("d", d3.geoPath())
+      .on("mouseover", d =>
+        d3
+          .select("#tooltip")
+          .attr("data-education", metadata.get(d.id).graduated)
+          .text(tooltip(metadata.get(d.id)))
+      );
 
     const legend = figure
       .append("svg")
@@ -99,8 +131,8 @@ async function getData(topologyURL, metadataURL) {
   };
 }
 
-function tooltip(d) {
-  return "";
+function tooltip({ state, county, graduated }) {
+  return `${county}, ${state}: ${graduated}%`;
 }
 
 if (process.env.NODE_ENV !== "test") {
